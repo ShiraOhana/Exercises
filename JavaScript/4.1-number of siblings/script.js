797b1f65029730db1a4ffc9a077b7f87252f31a2
ShiraOhana <soshiraohana@gmail.com>
2022-04-06 20:02:25 +0300

    solution to 4.1

---
+++ JavaScript/4.1-number of siblings/script.js
@@ -0,0 +1,9 @@
+let numSiblings = prompt("How many siblings do you have?");
+if (numSiblings == 1) {
+  console.log("One sibling!");
+}
+if (numSiblings > 1) {
+  console.log("More than 1 sibling");
+} else {
+  console.log("No siblings");
+}
