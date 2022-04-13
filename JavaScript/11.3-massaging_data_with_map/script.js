2da78e87fa3aadbf6a031de1d86eb9975308e1e1
ShiraOhana <soshiraohana@gmail.com>
2022-04-13 23:51:50 +0300

    11.3 in progress

---
+++ JavaScript/11.3-massaging_data_with_map/script.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    name: "John",
+    birthday: "1-1-1995",
+    favoriteFoods: {
+      meats: ["hamburgers", "sausages"],
+      fish: ["salmon", "pike"],
+    },
+  },
+  {
+    name: "Mark",
+    birthday: "10-5-1980",
+    favoriteFoods: {
+      meats: ["hamburgers", "steak", "lamb"],
+      fish: ["tuna", "salmon", "barracuda"],
+    },
+  },
+  {
+    name: "Mary",
+    birthday: "1-10-1977",
+    favoriteFoods: {
+      meats: ["ham", "chicken"],
+      fish: ["pike"],
+    },
+  },
+  {
+    name: "Thomas",
+    birthday: "1-10-1990",
+    favoriteFoods: {
+      meats: ["bird", "rooster"],
+      fish: ["salmon"],
+    },
+  },
+  {
+    name: "Mary",
+    birthday: "1-10-1977",
+    favoriteFoods: {
+      meats: ["hamburgers", "lamb"],
+      fish: ["anchovies", "tuna"],
+    },
+  },
+];
+
+// 1
+const names = (arr) => {
+  return arr.map((eachName) => ({ name: eachName.name }));
+};
+console.log(names(data));
+
+// 2
+const age = (str) => {
+  return str.replace("'", "");
+  //parsInt
+  //   eachAge.birthday
+};
+console.log(age(data));
